8e54bb36a176ed2e93949333b9a32cf9fffef972
jankiel325-create <jankiel325@gmail.com>
2026-08-09 04:52:52 -0500

    Connect public Sonix to trained model

---
+++ _next/static/chunks/04wwmuq1v-16w.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,18152,t=>{"use strict";let e=(0,t.i(19912).t)((t,e)=>{e.exports={}})();t.s(["default",0,e])}]);
